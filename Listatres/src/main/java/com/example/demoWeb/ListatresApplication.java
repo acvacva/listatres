d0985958b8454363b0aa6aefdb68f2f726087daa
sirivera <sirivera@N-5CG62309QR.nsn-intra.net>
2020-01-12 14:49:11 +0100

    first upload

---
+++ Listatres/src/main/java/com/example/demoWeb/ListatresApplication.java
@@ -0,0 +1,13 @@
+package com.example.demoWeb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ListatresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ListatresApplication.class, args);
+	}
+
+}
